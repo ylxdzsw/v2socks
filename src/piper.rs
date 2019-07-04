7c9191ef6d84e3642d8ea8d96bf6a06038c4adf1
ylxdzsw <ylxdzsw@gmail.com>
2019-07-04 17:56:54 +0800

    piper

--- src/piper.rs
+++ src/piper.rs
@@ -3,49 +3,70 @@ use std::io::prelude::*;
 use oh_my_rust::*;
 use std::time::Duration;
 
-struct Task {
-    from: Box<dyn ReadExt + Send>,
-    to: Box<dyn Write + Send>,
-    pipe: fn(&mut dyn ReadExt, &mut dyn Write),
-    end: fn(&mut dyn ReadExt, &mut dyn Write)
-}
+type Callback = Box<dyn FnMut() + Send>;
 
 /// A piper starts a background thread that do the piping work via mio
-#[allow(deprecated)]
 struct Piper {
-    tx: mio::channel::Sender<Task>
+    poll: mio::Poll,
+    callback_table: std::sync::Mutex<CallbackTable>
+}
+
+struct CallbackTable {
+    table: std::collections::BTreeMap<mio::Token, Callback>,
+    id: usize
 }
 
 impl Piper {
-    fn new() -> Piper {
-        #[allow(deprecated)]
-        let (tx, rx) = mio::channel::channel();
-
-        std::thread::spawn(move || {
-            let poll = match mio::Poll::new() {
+    // create and leak a Piper
+    pub fn new() -> &'static Piper {
+        let piper = Piper {
+            poll: match mio::Poll::new() {
                 Ok(poll) => poll,
                 Err(e) => panic!("failed to create Poll instance; err={:?}", e),
-            };
-            
+            },
+            callback_table: std::sync::Mutex::new(CallbackTable {
+                table: std::collections::BTreeMap::new(),
+                id: 0
+            }),
+        };
+
+        let piper = Box::leak(Box::new(piper));
+
+        let piper_copy = &*piper;
+        std::thread::spawn(move || {
             let mut events = mio::Events::with_capacity(1024);
 
-            let token_table = std::collections::BTreeMap::<mio::Token, Task>::new();
-
-            poll.register(&rx, mio::Token(0), mio::Ready::readable(), mio::PollOpt::edge());
-
-            let mut token_id = 1; // the zero-th token is for the channel receiver
-
             loop {
-                poll.poll(&mut events, None).unwrap();
+                piper_copy.poll.poll(&mut events, None).unwrap();
                 for event in &events {
-                    match event.token() {
-                        mio::Token(0) => unimplemented!(),
-                        token => unimplemented!()
-                    }
+                    let table = &mut piper_copy.callback_table.lock().unwrap().table;
+                    let f = table.get_mut(&event.token()).unwrap();
+                    f()
                 }
             }
         });
 
-        Piper { tx }
+        piper
+    }
+
+    pub fn register<R, W>(&self, mut from: R, mut to: W, pipe: impl Fn(&[u8], &mut W), end: impl Fn(&mut W))
+        where R: ReadExt + mio::Evented + Send + 'static,
+              W: Write + Send + 'static
+    {
+        let handle = &from as *const R;
+        let token = self.callback_table.lock().unwrap().insert(Box::new(move || {
+            std::io::copy(&mut from, &mut to);
+        }));
+        // unsafe: we are sure that the function won't be used before we register `from` to the poll
+        self.poll.register(unsafe { &*handle }, token, mio::Ready::readable(), mio::PollOpt::edge()).unwrap();
+    }
+}
+
+impl CallbackTable {
+    fn insert(&mut self, f: Callback) -> mio::Token {
+        let token = mio::Token(self.id);
+        self.id += 1;
+        self.table.insert(token, f);
+        token
     }
 }
